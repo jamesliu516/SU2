16dc8ac88aeb3b8003d8bbf61a3d3327af03e7f5
Jianming Liu <jmliu@jsnu.edu.cn>
2017-11-07 10:54:25 +0800

    In Sugon Machine add a new line.

--- SU2_CFD/src/SU2_CFD.cpp
+++ SU2_CFD/src/SU2_CFD.cpp
@@ -105,6 +105,7 @@ int main(int argc, char *argv[]) {
 
     /*--- Multi-zone problem: instantiate the multi-zone driver class by default
     or a specialized driver class for a particular multi-physics problem. ---*/
+	  /*--- For basic CFD simulation. ---*/ 
 
     driver = new CFluidDriver(config_file_name, nZone, nDim, MPICommunicator);
 
